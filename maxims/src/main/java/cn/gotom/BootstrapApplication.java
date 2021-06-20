961ace6f78bba63f56a3f900cba6a0631445dea6
peishaoguo <peishaoguo>
2021-06-21 01:36:26 +0800

    admin
    Signed-off-by: peishaoguo <peishaoguo>

---
+++ maxims/src/main/java/cn/gotom/BootstrapApplication.java
@@ -0,0 +1,19 @@
+package cn.gotom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@SpringBootApplication
+@ComponentScan({ "com.zhnaste" })
+public class BootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapApplication.class, args);
+	}
+
+}
---
+++ maxims/src/main/java/cn/gotom/SwaggerConfiguration.java
@@ -0,0 +1,56 @@
+package cn.gotom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import cn.gotom.commons.model.Token;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration {
+	@Bean
+	public Docket swaggerApi() {
+		List<RequestParameter> pars = new ArrayList<>();
+		RequestParameterBuilder builder = new RequestParameterBuilder();
+		builder.name(Token.ACCESS)//
+				.description("token")//
+				.in(ParameterType.HEADER)//
+				.required(false)//
+				.build(); //
+		pars.add(builder.build());
+		builder = new RequestParameterBuilder();
+		builder.name(Token.REFRESH)//
+				.description("token")//
+				.in(ParameterType.HEADER)//
+				.required(false)//
+				.build(); //
+		pars.add(builder.build());
+		return new Docket(DocumentationType.OAS_30)//
+				.apiInfo(swaggerApiInfo()).select()//
+				.apis(RequestHandlerSelectors.basePackage(CustomException.class.getPackageName()))//
+				.apis(RequestHandlerSelectors.any())//
+				.paths(PathSelectors.any())//
+				.build()//
+				.globalRequestParameters(pars);
+	}
+
+	private ApiInfo swaggerApiInfo() {
+		return new ApiInfoBuilder().title("API doc")//
+				.description("how to use this")//
+				.termsOfServiceUrl("https://localhost")
+				.contact(new Contact("xere", "https://localhost", "[email protected]"))//
+				.version("1.0").build();
+	}
+}
